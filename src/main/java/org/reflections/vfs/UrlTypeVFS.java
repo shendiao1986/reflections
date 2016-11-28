a7b6a8b74a32c1e0dcd89b2ec7c59fe1845c1f9c
shendiao1986 <teclusky@gmail.com>
2016-11-28 14:32:36 +0800

    fix multiple threds zip jar issues and jboss classes direcotry can't scanned issues

--- src/main/java/org/reflections/vfs/UrlTypeVFS.java
+++ src/main/java/org/reflections/vfs/UrlTypeVFS.java
@@ -16,103 +16,116 @@ import org.reflections.vfs.Vfs.UrlType;
 import com.google.common.base.Predicate;
 
 /**
- * UrlType to be used by Reflections library.
- * This class handles the vfszip and vfsfile protocol of JBOSS files.
+ * UrlType to be used by Reflections library. This class handles the vfszip and
+ * vfsfile protocol of JBOSS files.
  * <p>
- * <p>to use it, register it in Vfs via {@link org.reflections.vfs.Vfs#addDefaultURLTypes(org.reflections.vfs.Vfs.UrlType)} or {@link org.reflections.vfs.Vfs#setDefaultURLTypes(java.util.List)}.
+ * <p>
+ * to use it, register it in Vfs via
+ * {@link org.reflections.vfs.Vfs#addDefaultURLTypes(org.reflections.vfs.Vfs.UrlType)}
+ * or {@link org.reflections.vfs.Vfs#setDefaultURLTypes(java.util.List)}.
+ * 
  * @author Sergio Pola
  *
  */
 public class UrlTypeVFS implements UrlType {
-    public final static String[] REPLACE_EXTENSION = new String[]{".ear/", ".jar/", ".war/", ".sar/", ".har/", ".par/"};
+	public final static String[] REPLACE_EXTENSION = new String[] { ".ear/", ".jar/", ".war/", ".sar/", ".har/",
+			".par/" };
 
-    final String VFSZIP = "vfszip";
-    final String VFSFILE = "vfsfile";
+	final String VFSZIP = "vfszip";
+	final String VFSFILE = "vfsfile";
 
-    public boolean matches(URL url) {
-        return VFSZIP.equals(url.getProtocol()) || VFSFILE.equals(url.getProtocol());
-    }
+	public boolean matches(URL url) {
+		return VFSZIP.equals(url.getProtocol()) || VFSFILE.equals(url.getProtocol());
+	}
 
-    public Dir createDir(final URL url) {
-        try {
-            URL adaptedUrl = adaptURL(url);
-            return new ZipDir(new JarFile(adaptedUrl.getFile()));
-        } catch (Exception e) {
-            try {
-                return new ZipDir(new JarFile(url.getFile()));
-            } catch (IOException e1) {
-                if (Reflections.log != null) {
-                    Reflections.log.warn("Could not get URL", e);
-                    Reflections.log.warn("Could not get URL", e1);
-                }
-            }
-        }
-        return null;
-    }
+	public Dir createDir(final URL url) {
+		try {
+			URL adaptedUrl = adaptURL(url);
+			return new ZipDir(new JarFile(adaptedUrl.getFile()));
+		} catch (Exception e) {
+			try {
+				return new ZipDir(new JarFile(url.getFile()));
+			} catch (IOException e1) {
+				try {
+					URL adaptedUrl = adaptURL(url);
+					if (Vfs.getFile(adaptedUrl).isDirectory()) {
+						return new SystemDir(Vfs.getFile(adaptedUrl));
+					}
+				} catch (Exception e2) {
+					if (Reflections.log != null) {
+						Reflections.log.warn("Could not get URL", e);
+						Reflections.log.warn("Could not get URL", e1);
+						Reflections.log.warn("Could not get URL", e2);
+					}
+				}
+			}
+		}
+		return null;
+	}
 
-    public URL adaptURL(URL url) throws MalformedURLException {
-        if (VFSZIP.equals(url.getProtocol())) {
-            return replaceZipSeparators(url.getPath(), realFile);
-        } else if (VFSFILE.equals(url.getProtocol())) {
-            return new URL(url.toString().replace(VFSFILE, "file"));
-        } else {
-            return url;
-        }
-    }
+	public URL adaptURL(URL url) throws MalformedURLException {
+		if (VFSZIP.equals(url.getProtocol())) {
+			return replaceZipSeparators(url.getPath(), realFile);
+		} else if (VFSFILE.equals(url.getProtocol())) {
+			return new URL(url.toString().replace(VFSFILE, "file"));
+		} else {
+			return url;
+		}
+	}
 
-    URL replaceZipSeparators(String path, Predicate<File> acceptFile)
-            throws MalformedURLException {
-        int pos = 0;
-        while (pos != -1) {
-            pos = findFirstMatchOfDeployableExtention(path, pos);
+	URL replaceZipSeparators(String path, Predicate<File> acceptFile) throws MalformedURLException {
+		int pos = 0;
+		while (pos != -1) {
+			pos = findFirstMatchOfDeployableExtention(path, pos);
 
-            if (pos > 0) {
-                File file = new File(path.substring(0, pos - 1));
-                if (acceptFile.apply(file)) { return replaceZipSeparatorStartingFrom(path, pos); }
-            }
-        }
+			if (pos > 0) {
+				File file = new File(path.substring(0, pos - 1));
+				if (acceptFile.apply(file)) {
+					return replaceZipSeparatorStartingFrom(path, pos);
+				}
+			}
+		}
 
-        throw new ReflectionsException("Unable to identify the real zip file in path '" + path + "'.");
-    }
+		throw new ReflectionsException("Unable to identify the real zip file in path '" + path + "'.");
+	}
 
-    int findFirstMatchOfDeployableExtention(String path, int pos) {
-        Pattern p = Pattern.compile("\\.[ejprw]ar/");
-        Matcher m = p.matcher(path);
-        if (m.find(pos)) {
-            return m.end();
-        } else {
-            return -1;
-        }
-    }
+	int findFirstMatchOfDeployableExtention(String path, int pos) {
+		Pattern p = Pattern.compile("\\.[ejprw]ar/");
+		Matcher m = p.matcher(path);
+		if (m.find(pos)) {
+			return m.end();
+		} else {
+			return -1;
+		}
+	}
 
-    Predicate<File> realFile = new Predicate<File>() {
-        public boolean apply(File file) {
-            return file.exists() && file.isFile();
-        }
-    };
+	Predicate<File> realFile = new Predicate<File>() {
+		public boolean apply(File file) {
+			return file.exists() && file.isFile();
+		}
+	};
 
-    URL replaceZipSeparatorStartingFrom(String path, int pos)
-            throws MalformedURLException {
-        String zipFile = path.substring(0, pos - 1);
-        String zipPath = path.substring(pos);
+	URL replaceZipSeparatorStartingFrom(String path, int pos) throws MalformedURLException {
+		String zipFile = path.substring(0, pos - 1);
+		String zipPath = path.substring(pos);
 
-        int numSubs = 1;
-        for (String ext : REPLACE_EXTENSION) {
-            while (zipPath.contains(ext)) {
-                zipPath = zipPath.replace(ext, ext.substring(0, 4) + "!");
-                numSubs++;
-            }
-        }
+		int numSubs = 1;
+		for (String ext : REPLACE_EXTENSION) {
+			while (zipPath.contains(ext)) {
+				zipPath = zipPath.replace(ext, ext.substring(0, 4) + "!");
+				numSubs++;
+			}
+		}
 
-        String prefix = "";
-        for (int i = 0; i < numSubs; i++) {
-            prefix += "zip:";
-        }
+		String prefix = "";
+		for (int i = 0; i < numSubs; i++) {
+			prefix += "zip:";
+		}
 
-        if (zipPath.trim().length() == 0) {
-            return new URL(prefix + "/" + zipFile);
-        } else {
-            return new URL(prefix + "/" + zipFile + "!" + zipPath);
-        }
-    }
+		if (zipPath.trim().length() == 0) {
+			return new URL(prefix + "/" + zipFile);
+		} else {
+			return new URL(prefix + "/" + zipFile + "!" + zipPath);
+		}
+	}
 }
